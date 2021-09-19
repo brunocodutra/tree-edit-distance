95444cb8116411e1b938d0c851f81901b1483889
Bruno Dutra <brunocodutra@gmail.com>
2021-09-19 20:36:41 +0200

    improve benchmark

--- benches/diff.rs
+++ benches/diff.rs
@@ -1,9 +1,12 @@
-use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
-use itertools::Itertools;
+use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
+use derive_more::From;
+use proptest::strategy::{LazyJust, ValueTree};
+use proptest::{collection::vec, prelude::*, test_runner::TestRunner};
 use tree_edit_distance::{diff, Node, Tree};
 
-#[derive(Default, Clone)]
+#[derive(Debug, From)]
 struct TreeNode {
+    weight: u8,
     children: Vec<Self>,
 }
 
@@ -11,9 +14,9 @@ impl<'n> Node<'n> for TreeNode {
     type Kind = ();
     fn kind(&'n self) -> Self::Kind {}
 
-    type Weight = usize;
+    type Weight = u32;
     fn weight(&'n self) -> Self::Weight {
-        1
+        self.weight.into()
     }
 }
 
@@ -25,30 +28,26 @@ impl<'t> Tree<'t> for TreeNode {
     }
 }
 
-fn tree(leaves: Vec<TreeNode>, r: usize) -> TreeNode {
-    if leaves.len() < r {
-        TreeNode { children: leaves }
-    } else {
-        let n = (leaves.len() + r - 1) / r;
-        TreeNode {
-            children: leaves
-                .into_iter()
-                .chunks(n)
-                .into_iter()
-                .map(|c| tree(c.collect(), r))
-                .collect(),
-        }
-    }
+fn tree(depth: u32, breadth: u32) -> impl Strategy<Value = TreeNode> {
+    let size = (breadth.pow(depth + 1) - 1) / (breadth - 1) / 2; // half the maximum number of nodes
+    (1u8.., LazyJust::new(Vec::new))
+        .prop_map_into()
+        .prop_recursive(depth, size, breadth, move |inner| {
+            (1u8.., vec(inner, ..=breadth as usize)).prop_map_into()
+        })
 }
 
 fn bench(c: &mut Criterion) {
-    let mut group = c.benchmark_group("n-tree diff");
-    for r in [4, 8, 16] {
-        group.bench_with_input(
-            BenchmarkId::from_parameter(r),
-            &tree(vec![TreeNode::default(); 100], r),
-            |b, t| b.iter(|| diff(t, t)),
-        );
+    let mut runner = TestRunner::default();
+    let mut group = c.benchmark_group("n-ary tree diff");
+    for (d, b) in [(7, 2), (3, 6), (2, 15), (1, 255)] {
+        group.bench_with_input(format!("depth={}/breadth={}", d, b), &tree(d, b), |b, s| {
+            b.iter_batched_ref(
+                || (s, s).new_tree(&mut runner).unwrap().current(),
+                |(a, b)| diff(a, b),
+                BatchSize::SmallInput,
+            )
+        });
     }
     group.finish();
 }
